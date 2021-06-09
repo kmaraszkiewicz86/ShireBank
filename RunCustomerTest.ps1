8ae67633dc62c3ebf275b09cfce4f04fb598580f
krzysztof maraszkiewicz <krzysztof.maraszkiewicz.ext@bridgestone.eu>
2021-06-09 16:34:10 +0200

    Added powershell scripts

---
+++ RunCustomerTest.ps1
@@ -0,0 +1,19 @@
+﻿Write-Output ‘Step1: Start executing script’
+
+cd ./CustomerTest
+
+Write-Output ‘Step2: Restore nuget packages’
+
+dotnet restore
+
+Write-Output ‘Step3: Build application’
+
+dotnet build --configuration Release
+
+Write-Output ‘Step4: Run application’
+
+dotnet run --configuration Release
+
+Write-Output ‘Press any key to exit’
+
+pasue
---
+++ RunTests.ps1
@@ -0,0 +1,13 @@
+﻿Write-Output ‘Step1: Building application’
+
+cd .\Repository.Tests\
+
+dotnet restore
+dotnet build --configuration Debug
+
+Write-Output ‘Step2: Running tests’
+dotnet test -l "console;verbosity=detailed"
+
+Write-Output ‘Press any key to exit’
+
+pause
