f2a8fcd8a627cee1b2a92dd12bb8ded974aca173
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2021-06-15 06:48:33 +0200

    Added fixes to an Inspector logic

--- Services/Services/Implementations/Services/InspectorBlockerService.cs
+++ Services/Services/Implementations/Services/InspectorBlockerService.cs
@@ -1,25 +1,26 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using Services.Services.Interfaces.Services;
 
 namespace Services.Services.Implementations.Services
 {
     public sealed class InspectorBlockerService : IInspectorBlockerService
     {
-        private bool _shouldWaitForSignal = false;
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
 
         public void Block()
         {
-            _shouldWaitForSignal = true;
+            _cancellationTokenSource?.Cancel();
         }
 
         public void ReleaseLock()
         {
-            _shouldWaitForSignal = false;
+            _cancellationTokenSource = new CancellationTokenSource();
         }
 
         public async Task WaitWhenInspectionIsActiveAsync()
         {
-            while (_shouldWaitForSignal)
+            while (_cancellationTokenSource.Token.IsCancellationRequested)
             {
                 await Task.Delay(10);
             }
